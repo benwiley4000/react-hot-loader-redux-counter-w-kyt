c1c4599e754ae1e034e25f1ee6c1eb0114bf5480
Ben Wiley <therealbenwiley@gmail.com>
2016-10-19 00:41:54 -0400

    Count state kept in redux store.

--- src/client/index.js
+++ src/client/index.js
@@ -3,15 +3,32 @@
 // kyt expects every app to have this entry point.
 
 import React from 'react';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
 import App from './../components/App';
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'COUNT':
+      return Object.assign({}, state, {
+        count: state.count + 1
+      });
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 const root = document.querySelector('#root');
 
 const mount = RootComponent => render(
   <AppContainer>
-    <RootComponent />
+    <Provider store={store}>
+      <RootComponent />
+    </Provider>
   </AppContainer>,
   root
 );
