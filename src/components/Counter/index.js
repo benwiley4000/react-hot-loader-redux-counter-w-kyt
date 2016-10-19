f572ce07d92dfa7b0f09ef2ac14852448a099c53
Ben Wiley <therealbenwiley@gmail.com>
2016-10-19 19:48:13 -0400

    CounterContainer counts and passes state to Counter.

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import styles from './Counter.css';
 
-const Counter = () => (
-  <div className={styles.counter}>0</div>
+const Counter = ({ count }) => (
+  <div className={styles.counter}>{count}</div>
 );
 
-export default Counter;
+class CounterContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <Counter count={this.state.count} />
+  }
+}
+
+export default CounterContainer;
